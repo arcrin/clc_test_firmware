10dadefd3ae18f3ecbad9d0c80960efacafa88c3
Wen <wbai@deltacontrols.com>
2024-07-16 21:43:17 -0700

    uart driver

--- firmwareutil/hal/src/UARTConfig.h
+++ firmwareutil/hal/src/UARTConfig.h
@@ -2,29 +2,28 @@
 #include <cstdint>
 #include "GPIODriver.h"
 
-/// UART peripheral / bus configuration
 struct UARTConfig {
 	enum class PARITY : uint8_t {
-		NONE=0,
-		ODD=1,
-		EVEN=2,
+		NONE = 0,
+		ODD = 1,
+		EVEN = 2,
 	};
 
 	enum class STOP : uint8_t {
-		ONE=0,
-		TWO=1,
+		ONE = 0,
+		TWO = 1,
 	};
 
-	/// Baud rate
-	uint32_t baud;
-	/// Parity setting
+	constexpr UARTConfig(
+		uint32_t baudrate,
+		PARITY parity,
+		STOP stop
+	): baudrate(baudrate),
+	   parity(parity),
+	   stop(stop)
+	{}
+
+	uint32_t baudrate;
 	PARITY parity;
-	/// Number of stop bits
 	STOP stop;
-	/// IO pin to use for TX. Must be constructed with the appropriate alternate function
-	const GPIODriver TX;
-	/// IO pin to use for RX. Must be constructed with the appropriate alternate function
-	const GPIODriver RX;
-	/// Optional IO pin to use for RS485 RTS. Must be constructed as an output
-	const GPIODriver * RTS=nullptr;
 };
--- firmwareutil/hal/src/UARTDriver.h
+++ firmwareutil/hal/src/UARTDriver.h
@@ -1,101 +1,74 @@
 #pragma once
-#include "util.h"
 #include "UARTConfig.h"
-#include "UARTHAL.h"
-#include "RingBuffer.h"
 #include <string_view>
-#include <utility>
 
-/// An instance of this class represents the UART peripheral only
-/// The peripheral is routed to IO pins by using a UARTConfig instance
+
 class UARTDriver {
-	// TODO: UPDIDriver accesses `current_config` to change TX mode
-	friend class UPDIDriver;
-public:
-	/// Create a UART instance
-	/// \param hal Processor specific information about the UART peripheral
-	/// \param Buffer Data buffer to use for reception
-	/// \param BufferSize Size of the reception buffer
-	constexpr UARTDriver(UARTHAL hal, uint8_t *Buffer, uint32_t BufferSize) :
-		hal(std::move(hal)), Buffer(Buffer, BufferSize)
-		{}
-	UARTDriver() = delete;
-	~UARTDriver() = default;
+	public:
+		constexpr UARTDriver() = default;
+		~UARTDriver() = default;	
 
-	/// Common initialization of the UART system
-	static void DriverInit();
+		virtual void configure(UARTConfig const & current_config) const = 0;
 
-	/// (Re-)configure the UART peripheral using the settings provided in config, and route it to the IO pins
-	///  provided in config
-	/// \param config Configuration to use
-	void configure(UARTConfig const & current_config) const;
+		virtual void unconfigure() const = 0;	
 
-	/// Receive interrupt handler. This should put bytes into the RX buffer
-	void IRQHandler() const;
+		virtual void IRQHandler() const = 0;
 
-	/// Non-blocking receive character from RX buffer
-	/// \param[out] c Received character
-	/// \return True if a character was available. Otherwise, false is returned and `c` is invalid
-	bool ngetc(uint8_t &c) const;
+		virtual bool ngetc(uint8_t &c) const = 0;
 
-	/// Non-blocking receive string from RX buffer
-	/// \param buf Buffer to copy the string into
-	/// \param Length Maximum number of characters to receive
-	/// \return The number of characters that were available
-	size_t ngets(uint8_t * buf, size_t Length) const;
+		virtual bool ngetc(uint8_t) const = 0;
 
-	/// Transmit character
-	/// \param c Character to send out
-	void putc(char c) const;
+		virtual void enable_tx() const = 0;	
 
-	/// Transmit unsigned character
-	/// \param c Character to send out
-	void putc(uint8_t c) const;
+		virtual void disable_tx() const = 0;	
 
-	/// Enable / configure the TX IO pin for UART transmission
-	void enable_tx() const;
+		virtual void enable_rx() const = 0;
 
-	/// Disable / tri-state the TX IO pin
-	void disable_tx() const;
+		virtual void disable_rx() const = 0;
 
-	/// Enable / configure the RX IO pin for UART reception
-	void enable_rx() const;
+		/// @brief send a break condition for `nbits` bits	
+		/// @param nbits 
+		virtual void send_break(uint32_t nbits) const = 0;	
 
-	/// Disable / tri-state the RX IO pin
-	void disable_rx() const;
+		virtual void start_break() const = 0;	
 
-	/// Wait for transmission to finish
-	void flush() const;
+		virtual void end_break() const = 0;		
 
-	/// Receive any pending bytes and clear the RX buffer
-	void flush_input() const;
+		/// @brief wait for transmission to finish
+		virtual void flush() const = 0;
 
-	/// Transmit string_view
-	/// \param s String to send
-	void puts(std::string_view s) const;
+		/// @brief receive any pending bytes and clear the RX buffer
+		virtual void flush_input() const = 0;
 
-	/// Transmit character array
-	/// \param s Pointer to first character
-	/// \param Length Number of characters to transmit
-	void puts(const char * s, uint32_t Length) const;
+		/// @brief transmit unsigned character
+		/// @param c 
+		virtual void putc(uint8_t c) const = 0;
 
-	/// Transmit unsigned character array
-	/// \param s Pointer to first character
-	/// \param Length Number of characters to transmit
-	void puts(const uint8_t * s, uint32_t Length) const;
+		size_t ngets(uint8_t * buf, size_t length) const;
 
-private:
-	/// Transmit unsigned character, without asserting RTS
-	/// \param c Character to send out
-	void putc_raw(uint8_t c) const;
+		void putc(char c) const;
 
-	/// Assert the RTS signal, if it is present
-	void assert_rts() const;
+		/// @brief transmit string_view
+		/// @param s 
+		void puts(std::string_view s) const;
 
-	/// De-assert the RTS signal, if it is present
-	void deassert_rts() const;
+		/// @brief transmit character array
+		/// @param s 
+		/// @param length 
+		void puts(const char * s, uint32_t length) const;
 
-	const UARTHAL hal;
-	mutable const UARTConfig * current_config {};
-	const RingBuffer<uint8_t> Buffer;
+		/// @brief transmig unsigned character array
+		/// @param s 
+		/// @param length 
+		void puts(const uint8_t * s, uint32_t length) const;
+
+	protected:
+		/// @brief transmit unsigned character, without asserting RTS
+		/// @param c 
+		virtual void putc_raw(uint8_t c) const = 0;
+
+		/// @brief assert the RTS signal, if it is present
+		virtual void assert_rtx() const = 0;
+
+		virtual void deassert_rtx() const = 0;
 };

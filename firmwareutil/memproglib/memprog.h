1e848faa6aa091e73dff37d3b0ae8b0c89cd6c27
wbai <wbai@deltacontrols.com>
2024-07-19 19:09:04 -0700

    memprog

--- firmwareutil/memproglib/memprog.h
+++ firmwareutil/memproglib/memprog.h
@@ -24,5 +24,34 @@ typedef enum __attribute__((__packed__)) {
     // The target will set it ACK after copying the neecssary information
     _MEMPROG_STATUS_START = 0x01,
     // Set by the target along with the interface number to indicate that a command has been received.
-    // 
-};
+    // This lets the host know that it can set status back to IDLE and start transferring data for this interface.
+    _MEMPROG_STATUS_ACK = 0x02,
+    
+    // Values below 0x40 are reserved. DO not use. Values >= 0x40 are return statuses
+
+    // All other status codes are set by the command handlers upon completion of a command. They indicate
+    // to the host that the command is complete and return data should be read out.
+    // The host will set it back to IDLE after copying necessary data from params.
+    MEMPROG_STATUS_OK                   = 0x40,
+    MEMPROG_STATUS_ERR_PARAM            = 0x41,
+    MEMPROG_STATUS_ERR_EXECUTION        = 0x42,
+    MEMPROG_STATUS_ERR_TIMEOUT          = 0x43,
+
+    MEMPROG_STATUS_ERR_IMPLEMENTATION   = 0x7E,
+    MEMPROG_STATUS_ERR_OTHER            = 0x7F,
+
+    // 0x80 - 0xFF should only be used internally. Memprog may overwrite the original status with one of these values.
+    // In this case the original status will be shifted onto the bottom of Param.Code
+
+    // An error was detected in `GetTXStream`
+    _MEMPROG_STATUS_ERR_TX_STREAM       = 0x80,
+    // An error was detected in `WriteStream`
+    _MEMPROG_STATUS_ERR_WRITE_STREAM    = 0x81,
+    // An error was detected in `GetRXStream`
+    _MEMPROG_STATUS_ERR_RX_STREAM       = 0x82,
+    // An error was detected in `ReadStream`
+    _MEMPROG_STATUS_ERR_READ_STREAM     = 0x83,
+    // An error was detected in `ConvertStream`
+    _MEMPROG_STATUS_ERR_CONVERT_STREAM  = 0x84,
+} MEMPROG_STATUS;
+
